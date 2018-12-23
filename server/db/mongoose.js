30e5bdf626b7a93a84cfb4467fa5cfd20887b293
Alan Sanchez <sanchezalan95@gmail.com>
2018-12-23 15:24:41 -0600

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://todouser:alancool2304@ds243254.mlab.com:43254/todo-api', { useNewUrlParser: true }); //mongodb://<dbuser>:<dbpassword>@ds243254.mlab.com:43254/todo-api
 
 module.exports = {
     mongoose
